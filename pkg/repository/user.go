3504ab45ffc4ce1d2c79138e5f6f6316b6d8a4c4
lifina <jiei@omnisinc.co>
2019-04-18 13:39:01 +0900

    fix(entity): delete entity

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -27,3 +27,26 @@ func (u *user) Get(ctx context.Context, id int64) (*model.User, error) {
 func (u *user) Create(ctx context.Context, user *model.User) error {
 	return nil
 }
+
+type userEntity struct {
+	ID   int64
+	Name string
+	Age  int64
+	Sex  string
+}
+
+func (u *userEntity) buildFromModel(m model.User) {
+	u.ID = m.ID
+	u.Name = m.Name
+	u.Age = m.Age
+	u.Sex = m.Sex
+}
+
+func (u *userEntity) generateModel() *model.User {
+	return &model.User{
+		ID:   u.ID,
+		Name: u.Name,
+		Age:  u.Age,
+		Sex:  u.Sex,
+	}
+}
